ff2af45e3f4ea62699db16ca360cf1d67f79f230
PARDUMAN SINGLA <121317205+PARDUMAN-SINGLA@users.noreply.github.com>
2024-01-11 02:56:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all the JS code as the new version of JavaScript, and there's no such thing as "use no strict";
+//once "use strict" is added then whole of the document is treated as the new version of JavaScript
+
+//alert(8+5)  //currently using node.js, thus, won't be able to run here, to use alert we have a different syntax, which we can not be used by us right now
+
+//console.log(5+6); console.log("SINGLA")  //not a good practice as there's no code readibility
+
+// number => upto 2 to the power of 52 or 53, stores number
+// bigint to store larger numbers
+// string => use double quotes ""
+// boolean => ture or false
+// null => standalone value/empty
+// undefined 
+// symbol => use for uniqueness in react and figma
+// object
+
+console.log(typeof "singla") // string
+console.log(typeof 42646) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
